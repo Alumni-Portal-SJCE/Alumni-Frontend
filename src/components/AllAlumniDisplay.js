eb2015c57f5c2210485383d9cc3a2455a0966460
Shomikghosh <59383763+Shomikghosh@users.noreply.github.com>
2022-11-02 00:20:45 +0530

    Modal added

--- src/components/AllAlumniDisplay.js
+++ src/components/AllAlumniDisplay.js
@@ -1,15 +1,6 @@
 import React, { useEffect,useState } from 'react'
 import {url} from '../../src/mainUrl';
 import '../../src/static/css/cards.css';
-import Dialog from '@mui/material/Dialog';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import CloseIcon from '@mui/icons-material/Close';
-import Box from '@mui/material/Box';
-import {  DialogContent, TextField } from "@mui/material";
-import {Typography} from "@mui/material";
-import { Form, Button } from 'react-bootstrap';
 import emailjs from '@emailjs/browser';
 import InfiniteScroll from 'react-infinite-scroll-component';
 import EachAlumni from './EachAlumni';
@@ -41,129 +32,15 @@ function AllAlumniDisplay() {
       const indexOfFirstPost=indexOfLastPost-postsPerPage;
       const currentPosts=alumni.slice(indexOfFirstPost,indexOfLastPost);
       const paginate=(pageNumber)=>setCurrentPage(pageNumber);
-      const [openeditStudentDetails, setopeneditStudentDetails] = useState(false);
-      const [sendcontact,setContact]=useState('');
       const paginateFront = () => setCurrentPage(currentPage + 1);
-        const paginateBack = () => setCurrentPage(currentPage - 1);
-      const handleCloseopeneditStudentDetails = () => {
-        setopeneditStudentDetails(false);
-      };
-      const handleClickopeneditStudentDetails = (oncamp) => {
-          setopeneditStudentDetails(true);  
-          setContact(oncamp);  
-      };
-      const [mail, setSendMail] = useState({
-        name: "",
-        email: "",
-        message: ""
-      });
-    
-      
-  const handleChange = (prop) => (event) => {
-    setSendMail({ ...mail, [prop]: event.target.value });
-  };
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
-  const [success, setSuccess] = useState('');
-  const [error, setError] = useState('');
-  useEffect(() => {
-        emailjs.init('zmxtLJK_mxc708L5m');
-    }, [])
-
+         const paginateBack = () => setCurrentPage(currentPage - 1);
    
+  
+ 
   return (
     <div>
-            
-         <h2 class="headerOur">Our Alumni</h2>
-         <Dialog
-          fullScreen
-          open={openeditStudentDetails}
-          onClose={handleCloseopeneditStudentDetails}
-          // TransitionComponent={Transition}
-        //   sx={{width:'100%',display:'flex',alignItems:'center',justifyContent:'center',flexDirection:'column'}}
-        >
-          <AppBar sx={{ position: 'relative' }}>
-          <Toolbar>
-            <IconButton
-              edge="start"
-              color="inherit"
-              onClick={handleCloseopeneditStudentDetails}
-              aria-label="close"
-            >
-              <CloseIcon />
-            </IconButton>
-            <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
-              Connect with {sendcontact.a_name}
-            </Typography>
-          </Toolbar>
-        </AppBar>
-          <DialogContent>
-          <div className='email-form container'>
-				<h2>Get in touch</h2>
-				<Form
-					onSubmit={async event => {
-						event.preventDefault();
-						setSuccess('');
-						setEmail('');
-
-						try {
-							await emailjs.send('service_dhf2prr', 'template_in3nfs6', {
-								from_name: name,
-								from_email: email,
-								reply_to: 'placementsjce2022@gmail.com',
-								message: message,
-							});
-							setSuccess("We've received your email!");
-							setEmail('');
-							setName('');
-							setMessage('');
-						} catch (ex) {
-							setError('An error occurred, please try again!'+ex);
-						}
-					}}>
-					<Form.Group controlId='name'>
-						<Form.Label>Name</Form.Label>
-						<Form.Control
-							value={name}
-							onChange={e => setName(e.currentTarget.value)}
-							type='text'
-							placeholder='Enter name'
-							required
-						/>
-					</Form.Group>
-					<Form.Group controlId='formBasicEmail'>
-						<Form.Label>Email address</Form.Label>
-						<Form.Control
-							value={email}
-							onChange={e => setEmail(e.currentTarget.value)}
-							type='email'
-							placeholder='Enter email'
-							required
-						/>
-					</Form.Group>
-
-					<Form.Group controlId='message'>
-						<Form.Label>Message</Form.Label>
-						<Form.Control
-							value={message}
-							onChange={e => setMessage(e.currentTarget.value)}
-							as='textarea'
-							rows={3}
-							required
-						/>
-					</Form.Group>
-					{success && <p className='success'>{success}</p>}
-					{error && <p className='error'>{error}</p>}
-
-					<Button variant='success' type='submit' class="submitt">
-						Submit
-					</Button>
-				</Form>
-			</div>
-          </DialogContent>
-          
-        </Dialog><div class="x">
+          <h3 style={{marginTop:'5vh'}}>Our Alumni</h3>
+          <div class="x">
         
             <div class="container">
             
--- src/components/EachAlumni.js
+++ src/components/EachAlumni.js
@@ -1,17 +1,113 @@
-import React from 'react'
+import React ,{useState} from 'react'
 import '../../src/static/css/cards.css';
 
+import Dialog from '@mui/material/Dialog';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
+
+import Box from '@mui/material/Box';
+import { Form, Button } from 'react-bootstrap';
+import {  DialogContent, TextField } from "@mui/material";
+import {Typography} from "@mui/material";
+
 function EachAlumni({alumni,loading}) {
+      const [mail, setSendMail] = useState({
+        name: "",
+        email: "",
+        message: ""
+      });
+      const [openeditStudentDetails, setopeneditStudentDetails] = useState(false);
+      const [sendcontact,setContact]=useState('');
+
+      const handleCloseopeneditStudentDetails = () => {
+        setopeneditStudentDetails(false);
+      };
+      const handleClickopeneditStudentDetails = (oncamp) => {
+          setopeneditStudentDetails(true);  
+          setContact(oncamp);  
+      };
+      
+  const handleChange = (prop) => (event) => {
+    setSendMail({ ...mail, [prop]: event.target.value });
+  };
     if(loading)
     {
         return <h2>Loading...</h2>
     }
     return (
-<div class="container" id="alumni">
+    <div class="container" id="alumni">
+    <Dialog
+          fullScreen
+          open={openeditStudentDetails}
+          onClose={handleCloseopeneditStudentDetails}
+          // TransitionComponent={Transition}
+        //   sx={{width:'100%',display:'flex',alignItems:'center',justifyContent:'center',flexDirection:'column'}}
+        >
+          <AppBar sx={{ position: 'relative' }}>
+          <Toolbar>
+            <IconButton
+              edge="start"
+              color="inherit"
+              onClick={handleCloseopeneditStudentDetails}
+              aria-label="close"
+            >
+              <CloseIcon />
+            </IconButton>
+            <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
+              Connect with {sendcontact.a_name}
+            </Typography>
+          </Toolbar>
+        </AppBar>
+          <DialogContent>
+          <TextField
+                      fullWidth
+                      label="Full Name"
+                      id="fullWidth"
+                      placeholder=""
+                      style={{ marginBottom: "1rem" }}
+                      value={mail.name}
+                      onChange={handleChange("name")}
+                    />
+            <TextField
+                      fullWidth
+                      label="Email"
+                      id="fullWidth"
+                      placeholder="Enter your email"
+                      style={{ marginBottom: "1rem" }}
+                      value={mail.email}
+                      onChange={handleChange("email")}
+                    />
+                <TextField
+                      fullWidth
+                      label="Message"
+                      id="fullWidth"
+                      placeholder="Enter your queries (don't ask questions which are confidential)"
+                      style={{ marginBottom: "1rem" }}
+                      value={mail.message}
+                      multiline
+                      rows={6}
+                      onChange={handleChange("message")}
+                    />
+                    <Box sx={{justifyContent: "center"}}>
+                        <Button
+                        variant="contained"
+                        style={{  }}
+                        // onClick={handleFeedbackSubmit}
+                        disabled={loading ? true : false}
+                    >
+                        SEND
+                    </Button>
+                    </Box>
+                    
+          </DialogContent>
+          
+        </Dialog>
     {alumni?.map(function (oncamp, i) {
                 return (
             <>            
- <div class="card">
+    <div class="card">
                 <div class="box">
                 <div class="content">
                     <h2>{oncamp.a_name.slice(0,1)}</h2>
@@ -19,7 +115,7 @@ function EachAlumni({alumni,loading}) {
                     <h3>{oncamp.a_cname}</h3>
                     <p>{oncamp.branch}</p>
                     <p>{oncamp.passout} Passout</p>
-                    {/* <button onClick={()=>{handleClickopeneditStudentDetails(oncamp)}}>Contact</button> */}
+                    <button onClick={()=>{handleClickopeneditStudentDetails(oncamp)}}>Contact</button>
                 </div>
                 </div>
             </div></> );
